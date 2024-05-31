19e6cb5dc2b47acef56d3d585ab5e7e5122be56e
daniel krokovsky <danielkrokovsky@hotmail.com>
2024-05-31 20:05:37 -0300

    ajuste

--- src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
+++ src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
@@ -1,9 +1,11 @@
 package com.polarbookshop.orderservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
